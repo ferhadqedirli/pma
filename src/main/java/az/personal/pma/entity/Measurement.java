4ace497679376aad0e02adbb1816994d63e4a7ab
Farhad <fgadirli@turanbank.az>
2023-01-16 18:01:17 +0400

    changes

--- src/main/java/az/personal/pma/entity/Measurement.java
+++ src/main/java/az/personal/pma/entity/Measurement.java
@@ -1,4 +1,21 @@
 package az.personal.pma.entity;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Entity
 public class Measurement {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 20)
+    @NotBlank
+    private String name;
 }
